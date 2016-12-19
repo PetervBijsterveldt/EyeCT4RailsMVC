87d422396e0649d3ed096a6165fd9500c34bd129
Lombarts,Arjan A.J.P.F. <a.lombarts@student.fontys.nl>
2016-12-19 13:17:48 +0100

    User class constructor wijzigingen

--- EyeCT4RailzMVC/Models/User.cs
+++ EyeCT4RailzMVC/Models/User.cs
@@ -9,21 +9,16 @@ namespace EyeCT4RailzMVC.Models
     {
         public int ID { get; set; }
         public string Naam { get; set; }
-        //public string Wachtwoord { get; set; }
-        //public string Email { get; set; }
-        //public string Postcode { get; set; }
-        //public string Woonplaats { get; set; }
-        //public string Rfid { get; set; }
         public UserType Rol { get; set; }
         
-        public User(int id, string naam)
-        {
-            ID = id;
-            Naam = naam;
-        }
         public User(int id, string naam, UserType rol)
         {
             ID = id;
+            Naam = naam;
+            Rol = rol;
+        }
+        public User(string naam, UserType rol)
+        {
             Naam = naam;
             Rol = rol;
         }
