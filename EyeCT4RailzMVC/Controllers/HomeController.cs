472749f373243fe4941873495253b11d66d67aee
Peter van Bijsterveldt <petervbijsterveldt@gmail.com>
2016-12-21 10:07:30 +0100

    Weet niet wat het was

--- EyeCT4RailzMVC/Controllers/HomeController.cs
+++ EyeCT4RailzMVC/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace EyeCT4RailzMVC.Controllers
             return View();
         }
 
-        [Authorize(Users = @"CONTOSO\Rick, CONTOSO\Keith, CONTOSO\Mike")]
+        [Authorize(Users = @"eyect4railz\administrator,CONTOSO\Rick, CONTOSO\Keith, CONTOSO\Mike")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
