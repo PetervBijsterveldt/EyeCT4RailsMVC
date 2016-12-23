d768aa4bd94cb1c705cfcf0ac7e4b5138c28d937
Jelle Schrader <j.schrader@student.fontys.nl>
2016-12-23 13:28:01 +0100

    nieuwe layout, facking mooi

--- EyeCT4RailzMVC/App_Start/BundleConfig.cs
+++ EyeCT4RailzMVC/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace EyeCT4RailzMVC
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-simplex.css",
                       "~/Content/site.css"));
         }
     }
