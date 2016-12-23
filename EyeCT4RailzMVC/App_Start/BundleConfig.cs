a82ac394223df1840ec0a194677b327e4143bca2
Jelle Schrader <j.schrader@student.fontys.nl>
2016-12-23 14:24:10 +0100

    paar layour changes (index page)

--- EyeCT4RailzMVC/App_Start/BundleConfig.cs
+++ EyeCT4RailzMVC/App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace EyeCT4RailzMVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include( 
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
